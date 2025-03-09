b9899e4bcbcc208ef27ad2a47edb60e35510a828
maxthrillerlive <maxthrillerlive@gmail.com>
2025-03-09 16:10:45 -0500

    feat: Add Russian greeting command (!privet)

---
+++ src/commands/privet.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'privet',
+    description: 'Say hello in Russian',
+    trigger: '!привет',
+    enabled: true,
+    modOnly: false,
+    execute: async (client, channel, userstate, message) => {
+        await client.say(channel, 'Привет! (Privet!)');
+    }
+}; 
